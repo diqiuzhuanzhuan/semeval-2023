fix calc average loss

task2/modeling/model.py
@@ -109,7 +109,7 @@ class BaselineNerModel(NerModel):
     def training_step(self, batch: Any, batch_idx: int):
         outputs = self.forward_step(batch)
         self.log_metrics(outputs['metric'], outputs['loss'], suffix='train_', on_step=True, on_epoch=False)
-        return outputs['loss']
+        return {'loss': outputs['loss']}
 
     def on_train_epoch_start(self) -> None:
         self.metric.reset()
@@ -124,14 +124,14 @@ class BaselineNerModel(NerModel):
     def validation_step(self, batch: Any, batch_idx: int):
         outputs = self.forward_step(batch)
         self.log_metrics(outputs['metric'], outputs['loss'], suffix='val_', on_step=True, on_epoch=False)
-        return outputs['loss']
+        return {'loss': outputs['loss']}
 
     def on_validation_epoch_start(self) -> None:
         self.metric.reset()
         return super().on_validation_epoch_start()
 
     def validation_epoch_end(self, outputs) -> None:
-        average_loss = torch.mean(torch.tensor(outputs, device=self.device))
+        average_loss = torch.mean(torch.tensor([item['loss'] for item in outputs], device=self.device))
         metric = self.metric.compute()
         self.log_metrics(metric, average_loss, suffix='val_', on_step=False, on_epoch=True)
         return super().validation_epoch_end(outputs)