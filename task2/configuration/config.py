32cebee6527a83967c9518b781a1498f50550922
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2022-10-21 09:34:55 +0800

    add test file config

--- task2/configuration/config.py
+++ task2/configuration/config.py
@@ -45,6 +45,21 @@ validate_file = {
 	'German': data_path/'semeval_2021_task_11_trial_data.txt',
 }
 
+test_file = {
+    'English': data_path/'semeval_2021_task_11_trial_data.txt',
+ 	'Spanish': data_path/'semeval_2021_task_11_trial_data.txt',
+    'Hindi': data_path/'semeval_2021_task_11_trial_data.txt',
+	'Bangla': data_path/'semeval_2021_task_11_trial_data.txt',
+	'Chinese': data_path/'semeval_2021_task_11_trial_data.txt',
+	'Swedish': data_path/'semeval_2021_task_11_trial_data.txt',
+	'Farsi': data_path/'semeval_2021_task_11_trial_data.txt',
+	'French': data_path/'semeval_2021_task_11_trial_data.txt',
+	'Italian': data_path/'semeval_2021_task_11_trial_data.txt',
+	'Portugese': data_path/'semeval_2021_task_11_trial_data.txt',
+	'Ukranian': data_path/'semeval_2021_task_11_trial_data.txt',
+	'German': data_path/'semeval_2021_task_11_trial_data.txt',
+}
+
 performance_log = log_path/'performance.csv'
 
 ###### for log ######
