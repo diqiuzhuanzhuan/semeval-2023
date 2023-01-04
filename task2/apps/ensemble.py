c180d0e3cde7f03b3c636e008f7b1162ec8ee892
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2023-01-04 17:42:36 +0800

    fix vote code

--- task2/apps/ensemble.py
+++ task2/apps/ensemble.py
@@ -6,16 +6,19 @@ from typing import List, AnyStr
 import numpy as np
 import os
 from sklearn.metrics import classification_report
+import itertools
 from task2.data_man.meta_data import get_id_by_type, get_type_by_id, LABEL_NAME, read_conll_item_from_file
 
 def vote(dev_label_files: List[AnyStr], dev_files: List[AnyStr], test_files: List[AnyStr], output_file):
     assert len(dev_files) == len(test_files) == len(dev_label_files)
     def _get_dev_report(dev_file, dev_label_file):
-        y_pred = [item.labels for item in read_conll_item_from_file(dev_file)]
-        y_true = [item.labels for item in read_conll_item_from_file(dev_label_file, just_label=True)]
+        y_pred = [item.labels for item in read_conll_item_from_file(dev_file, just_label=True)]
+        y_pred = list(itertools.chain(*y_pred))
+        y_true = [item.labels for item in read_conll_item_from_file(dev_label_file)]
+        y_true = list(itertools.chain(*y_true))
         length = min(len(y_true), len(y_pred))
         report_dict = classification_report(y_true=y_true[0:length], y_pred=y_pred[0:length], output_dict=True)
-        report_dict['O'] = report_dict['micro avg']
+        report_dict['O'] = report_dict['macro avg']
         return report_dict
 
     def _calc_score(test_file, report_dict, final_res=[]):
@@ -27,9 +30,8 @@ def vote(dev_label_files: List[AnyStr], dev_files: List[AnyStr], test_files: Lis
                     _tag = tag
                 else:
                     _tag = tag[2:]
-                score = report_dict[_tag]['f1-score']
-                #print(idx, jdx)
-                final_res[idx][jdx][get_id_by_type[tag]] += score
+                score = report_dict[tag]['f1-score']
+                final_res[idx][jdx][get_id_by_type(tag)] += score
         return final_res
 
     def _weight_sum(final_res):
@@ -40,9 +42,10 @@ def vote(dev_label_files: List[AnyStr], dev_files: List[AnyStr], test_files: Lis
         if not os.path.exists(os.path.dirname(output_file)):
             os.makedirs(os.path.dirname(output_file))
         with open(output_file, "w") as f:
+            f.write('\n')
             for l in final_y_pred:
                 f.write('\n'.join(l))
-                f.write("\n\n")
+                f.write("\n\n\n")
         
     final_res = []
     for dev_file, dev_label_file, test_file in zip(dev_files, dev_label_files, test_files):
@@ -51,3 +54,15 @@ def vote(dev_label_files: List[AnyStr], dev_files: List[AnyStr], test_files: Lis
     final_y_pred = _weight_sum(final_res)
     _write_vote_result(output_file, final_y_pred)
     return final_y_pred
+
+if __name__ == "__main__":
+    from task2.configuration import config
+    val_label_files = [config.validate_file['Chinese']]
+    val_preds_files = ['a.tsv']
+    test_files = ['val_micro@F1=0.6998722860791327.tsv']
+    output_file = './zh.pred.conll'
+    vote(val_label_files, val_preds_files, test_files, output_file)
+    for item1, item2 in zip(read_conll_item_from_file(output_file, just_label=True), read_conll_item_from_file(test_files[0], just_label=True)):
+        assert len(item1.labels) == len(item2.labels)
+        for l1, l2 in zip(item1.labels, item2.labels):
+            assert l1 == l2
